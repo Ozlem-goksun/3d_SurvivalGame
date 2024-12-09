88e8ab230b8c4323c457c122aaa8a297823f4823
dinosOnVenus <ozlemgoksun1389@gmail.com>
2024-12-09 16:02:17 +0300

    added mouse Movement

---
+++ 3d_SurvivalGame/Assets/Scripts/MouseMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour
+{
+    [SerializeField] private Vector2 turn;
+    [SerializeField] private float sensitivity = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LookAround();
+    }
+
+    private void LookAround()
+    {
+        turn.x += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        turn.y += Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
+    }
+}
